97ec6c7584c1d3c47204e856fafc13581adf9819
Bunni <7656604+BunniJ@users.noreply.github.com>
2022-07-08 21:19:15 +0200

    Update MainActivity.kt
    Finish test Part 2

--- app/src/main/java/com/example/aoc/MainActivity.kt
+++ app/src/main/java/com/example/aoc/MainActivity.kt
@@ -18,6 +18,9 @@ class MainActivity : AppCompatActivity() {
 
         // Part 1
         partOne()
+
+        //Part 2
+        partTwo()
     }
 
     private fun partOne() {
@@ -28,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         for (content in puzzleContent) {
             val chunkList = arrayListOf<Char>()
             for (illegalChar in content) {
-                when(illegalChar) {
+                when (illegalChar) {
                     // setup legal pairs
                     '(' -> chunkList += ')'
                     '[' -> chunkList += ']'
@@ -39,7 +42,7 @@ class MainActivity : AppCompatActivity() {
                             break
                         }
                         if (chunkList.removeLast() != illegalChar) {
-                            syntaxErrScore += when(illegalChar) {
+                            syntaxErrScore += when (illegalChar) {
                                 ')' -> 3
                                 ']' -> 57
                                 '}' -> 1197
@@ -53,6 +56,48 @@ class MainActivity : AppCompatActivity() {
             }
         }
         println("Total syntax error score of the first puzzle is: $syntaxErrScore")
+    }
+
+    private fun partTwo() {
+        val puzzleContent = puzzleInput.readLines()
+        val autoCompleteList = arrayListOf<Long>()
+
+        for (content in puzzleContent) {
+            val incompleteLines = arrayListOf<Char>()
+            var isComplete = true
+
+            for (char in content) {
+                when (char) {
+                    '(' -> incompleteLines += ')'
+                    '[' -> incompleteLines += ']'
+                    '{' -> incompleteLines += '}'
+                    '<' -> incompleteLines += '>'
+                    else -> {
+                        if (incompleteLines.isEmpty() || incompleteLines.removeLast() != char) {
+                            isComplete = false
+                            break
+                        }
+                    }
+                }
+            }
+            if (isComplete && incompleteLines.isNotEmpty()) {
+                var score = 0L
+                while (incompleteLines.isNotEmpty()) {
+                    score *= 5
+                    when (incompleteLines.removeLast()) {
+                        ')' -> score += 1
+                        ']' -> score += 2
+                        '}' -> score += 3
+                        '>' -> score += 4
+                    }
+                }
+                autoCompleteList += score
+            }
+        }
+        // sorting all of the scores
+        autoCompleteList.sort()
+        // taking the middle score
+        println("Total syntax error score of the second puzzle is: ${autoCompleteList[autoCompleteList.size / 2]}")
     }
 
     private fun createPuzzleInputFile() {
